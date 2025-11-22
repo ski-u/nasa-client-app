dbec565aa5ff93e5fed6201551cc7a394cc1e6fc
Yunosuke Sakai <qmkns246@gmail.com>
2025-11-22 17:39:38 +0900

    use `liveValue` for `apiClient`

--- App/NASAClientApp.swift
+++ App/NASAClientApp.swift
@@ -9,9 +9,7 @@ struct NASAClientApp: App {
             AppView(
                 store: .init(initialState: .init()) {
                     AppReducer().transformDependency(\.self) {
-                        // Use `mockValue` to return mock response due to the unavailabillity of NASA APIs. (as of 2025/10/16)
-                        // See https://github.com/nasa/apod-api/issues/139 for more detail.
-                        $0.apiClient = .mockValue
+                        $0.apiClient = .liveValue
                     }
                 }
             )
