e3c5f1c6d2c12570115bad52d3216b6db2811f4a
Yunosuke Sakai <qmkns246@gmail.com>
2026-02-14 17:10:36 +0900

    add fetch method to share logic

--- Features/Sources/APIClientLive/LiveKey.swift
+++ Features/Sources/APIClientLive/LiveKey.swift
@@ -10,40 +10,41 @@ extension APIClient: DependencyKey {
     private static let apiKeyClient = APIKeyClient.liveValue
     private static let baseURL = URL(string: "https://api.nasa.gov")!
     
-    // TODO: API request abstraction
     public static var liveValue: Self {
         .init(
             fetchAstronomyPictures: {
-                var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: false)!
-                urlComponents.path = "/planetary/apod"
-                urlComponents.queryItems = [
-                    URLQueryItem(name: "api_key", value: apiKeyClient.getKey()?.rawValue),
-                    URLQueryItem(
-                        name: "start_date",
-                        value: LocalDate().addingMonths(-1).description
-                    ),
-                ]
-                
-                let (data, _) = try await URLSession.shared.data(from: urlComponents.url!)
-                
-                return try JSONDecoder()
-                    .decode([AstronomyPicture.Payload].self, from: data)
+                let payloads: [AstronomyPicture.Payload] = try await fetch(
+                    path: "/planetary/apod",
+                    queryItems: [
+                        URLQueryItem(
+                            name: "start_date",
+                            value: LocalDate().addingMonths(-1).description
+                        ),
+                    ]
+                )
+                return payloads
                     .map(AstronomyPicture.init)
                     .reversed()
             },
             fetchTodayPicture: {
-                var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: false)!
-                urlComponents.path = "/planetary/apod"
-                urlComponents.queryItems = [
-                    URLQueryItem(name: "api_key", value: apiKeyClient.getKey()?.rawValue)
-                ]
-                
-                let (data, _) = try await URLSession.shared.data(from: urlComponents.url!)
-                
-                return try AstronomyPicture(
-                    payload: JSONDecoder().decode(AstronomyPicture.Payload.self, from: data)
-                )
+                let payload: AstronomyPicture.Payload = try await fetch(path: "/planetary/apod")
+                return AstronomyPicture(payload: payload)
             }
         )
+    }
+
+    private static func fetch<T: Decodable>(
+        path: String,
+        queryItems: [URLQueryItem] = []
+    ) async throws -> T {
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: false)!
+        urlComponents.path = path
+        urlComponents.queryItems = [
+            URLQueryItem(name: "api_key", value: apiKeyClient.getKey()?.rawValue)
+        ] + queryItems
+
+        let (data, _) = try await URLSession.shared.data(from: urlComponents.url!)
+
+        return try JSONDecoder().decode(T.self, from: data)
     }
 }
