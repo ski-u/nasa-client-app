e106cac206dfb7059a5e2b4627ed6238dbcedbd4
Yunosuke Sakai <qmkns246@gmail.com>
2026-02-21 23:17:04 +0900

    add caption to `TranslationView`

--- Features/Sources/Settings/Translation/TranslationView.swift
+++ Features/Sources/Settings/Translation/TranslationView.swift
@@ -7,40 +7,42 @@ struct TranslationView: View {
     
     var body: some View {
         Form {
-            Button(
-                action: {
-                    $isTranslationEnabled.withLock {
-                        $0 = true
+            Section(footer: Text("Supports translation into Japanese", bundle: .module)) {
+                Button(
+                    action: {
+                        $isTranslationEnabled.withLock {
+                            $0 = true
+                        }
+                    }
+                ) {
+                    HStack {
+                        Text("On", bundle: .module)
+                            .foregroundStyle(Color.primary)
+                        
+                        Spacer()
+                        
+                        if isTranslationEnabled {
+                            Image(systemName: "checkmark")
+                        }
                     }
                 }
-            ) {
-                HStack {
-                    Text("On", bundle: .module)
-                        .foregroundStyle(Color.primary)
-                    
-                    Spacer()
-                    
-                    if isTranslationEnabled {
-                        Image(systemName: "checkmark")
+                
+                Button(
+                    action: {
+                        $isTranslationEnabled.withLock {
+                            $0 = false
+                        }
                     }
-                }
-            }
-            
-            Button(
-                action: {
-                    $isTranslationEnabled.withLock {
-                        $0 = false
-                    }
-                }
-            ) {
-                HStack {
-                    Text("Off", bundle: .module)
-                        .foregroundStyle(Color.primary)
-                    
-                    Spacer()
-                    
-                    if !isTranslationEnabled {
-                        Image(systemName: "checkmark")
+                ) {
+                    HStack {
+                        Text("Off", bundle: .module)
+                            .foregroundStyle(Color.primary)
+                        
+                        Spacer()
+                        
+                        if !isTranslationEnabled {
+                            Image(systemName: "checkmark")
+                        }
                     }
                 }
             }
@@ -48,4 +50,8 @@ struct TranslationView: View {
         .navigationTitle(Text("Machine Translation", bundle: .module))
         .navigationBarTitleDisplayMode(.inline)
     }
+}
+
+#Preview {
+    TranslationView()
 }
